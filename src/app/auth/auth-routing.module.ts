e107821230431ffbe7c4e1a543af059bd5a317e5
Hector Toro <alejotoro94@hotmail.com>
2024-03-17 15:45:00 -0500

    Se agrega nuestras rutas iniciales, heroes, auth, 404 y el concepto de ruta hija en heroes con newPage

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPagesComponent } from './pages/layout-pages/layout-pages.component';
+import { NewPageComponent } from '../heroes/pages/new-page/new-page.component';
 
+// localhost:4200/auth
 const routes: Routes = [
-
+  {
+    path: '',
+    component: LayoutPagesComponent,
+    children: [
+      {
+        path: 'new-hero',
+        component: NewPageComponent
+      }
+    ]
+  },
 ];
 
 @NgModule({
