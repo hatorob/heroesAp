0b7441f2fd52433cc53d4725b0b0b0a61fa15c96
Hector Toro <alejotoro94@hotmail.com>
2024-03-19 18:12:28 -0500

    Se agrega el estilo de la página de hero

--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../../interfaces/hero.interface';
 import { HeroesService } from '../../services/heroes.service';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -24,12 +24,16 @@ export class HeroPageComponent implements OnInit {
   ngOnInit(): void {
     this.activatedRoute.params
       .pipe(
-        switchMap( ({id}) => this.herosService.getHeroById(id)),
+        switchMap( ({id}) => this.herosService.getHeroById(id))
       ).subscribe( hero => {
         if( !hero ) return this.router.navigate(['/heroes/list']);
         this.hero = hero;
         return;
       });
+  }
+
+  public goBack = (): void => {
+    this.router.navigateByUrl('heroes/list');
   }
 
 }
