e3f7432a8e6ddf87e83e0462ca096e0e6f764bd0
Hector Toro <alejotoro94@hotmail.com>
2024-03-18 21:12:20 -0500

    Se agrega la consulta heroes por id en services y en heroes-page.components.ts

--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-hero-page',
   templateUrl: './hero-page.component.html',
   styles: ``
 })
-export class HeroPageComponent {
+export class HeroPageComponent implements OnInit {
+
+  public hero!: Hero | undefined;
+
+  constructor(
+    private herosService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        switchMap( ({id}) => this.herosService.getHeroById(id)),
+      ).subscribe( hero => {
+        if( !hero ) return this.router.navigate(['/heroes/list']);
+        this.hero = hero;
+        return;
+      });
+  }
 
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from '../../../environments/environments';
 
@@ -15,6 +15,13 @@ export class HeroesService {
 
   public getHero = (): Observable<Hero[]> => {
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
+  }
+
+  public getHeroById = (id:string): Observable<Hero | undefined> => {
+    return this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`)
+                .pipe(
+                  catchError( error => of(undefined) )
+                );
   }
 
 }
