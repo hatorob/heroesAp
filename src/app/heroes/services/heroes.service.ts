9b2ee761a6f52c3fd9a1457d1ead4c17cefc17a8
Hector Toro <alejotoro94@hotmail.com>
2024-03-20 16:42:04 -0500

    Se agrega el CRUD completo en nuestro services

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, map, of, switchMap } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from '../../../environments/environments';
 
@@ -32,6 +32,42 @@ export class HeroesService {
               .pipe(
                 map( heroes => heroes.filter( ({superhero}) => superhero!.toLowerCase().includes(query.toLowerCase())))
               );
+  }
+
+  //! CRUD
+  //! add
+  /**
+   * Esta función se encargara de ingresar un nuevo heroe
+   * @param hero -> objeto de hero
+   * @returns
+   */
+  public addHero = (hero: Hero): Observable<Hero> => {
+    return this.http.post<Hero>(`${this.baseUrl}/heroes`,hero);
+  }
+
+  //! Update
+  /**
+   * Esta función se encarga de actualizar alguna propiedad de heroes
+   * @param hero objeto de hero
+   * @returns
+   */
+  public updateHero = (hero: Hero): Observable<Hero> => {
+    if( !hero.id ) throw Error('Hero id is required');
+    return this.http.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`,hero);
+  }
+
+  //! Delete
+  /**
+   * Esta función se encarga de eliminar heroes
+   * @param hero objeto de hero
+   * @returns
+   */
+  public delatedHero = (id: string): Observable<boolean> => {
+    return this.http.delete(`${this.baseUrl}/heroes/${id}`)
+                .pipe(
+                  catchError( error => of(false)),
+                  map( resp => true )
+                )
   }
 
 }
