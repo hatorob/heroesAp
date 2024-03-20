6bad8465d59ce7b89cd9a67fb83851a40344f75b
Hector Toro <alejotoro94@hotmail.com>
2024-03-19 20:22:42 -0500

    Se hace el autocompletado del buscador de heroes

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { HeroesService } from '../../services/heroes.service';
 import { FormControl } from '@angular/forms';
+import { Hero } from '../../interfaces/hero.interface';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-search-page',
@@ -10,8 +12,33 @@ import { FormControl } from '@angular/forms';
 export class SearchPageComponent {
 
   public searchInput = new FormControl('');
+  public heroes: Hero[] = [];
+  public selectedHero?: Hero;
 
   constructor( private heroesServices: HeroesService ) {
+
+  }
+
+  public searchHero = (): void => {
+
+    const value: string = this.searchInput.value || '';
+
+    this.heroesServices.getSuggestions(value)
+        .subscribe( heroes => this.heroes = heroes);
+
+  }
+
+  public onSelectedOption = (e: MatAutocompleteSelectedEvent): void => {
+
+    if(!e.option.value) {
+      this.selectedHero = undefined;
+      return;
+    }
+
+    const hero: Hero = e.option.value;
+    this.searchInput.setValue(hero.superhero);
+
+    this.selectedHero = hero;
 
   }
 
